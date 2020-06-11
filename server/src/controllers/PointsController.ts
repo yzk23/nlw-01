cc9ec0d778eff98b9b34b0d5685b9b9c67732c06
yzk23 <isaacoelhomedeiros@gmail.com>
2020-06-11 02:01:23 -0300

    consertando o upload de imagens

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -19,7 +19,7 @@ class PointsController {
     const serializedPoints = points.map((point) => {
       return {
         ...point,
-        image_url: `http://192.168.100.16:3333/uploads/${point.image}`,
+        image_url: `http://localhost:3333/uploads/${point.image}`,
       };
     });
 
@@ -37,7 +37,7 @@ class PointsController {
 
     const serializedPoint =  {
         ...point,
-        image_url: `http://192.168.100.16:3333/uploads/${point.image}`,
+        image_url: `http://localhost:3333/uploads/${point.image}`,
     };
 
     const items = await knex("items")
